b9c5a7d2bc668e257299a0177a8f432a019954df
eguvenc <eguvenc@gmail.com>
2017-09-20 21:48:14 +0300

    added path controller

--- src/Router.php
+++ src/Router.php
@@ -24,10 +24,7 @@ class Router implements RouterInterface
 
     protected $path;
     protected $group;
-    protected $class;
     protected $queue;
-    protected $folder;
-    protected $method;
     protected $handler;
     protected $request;
     protected $response;
@@ -283,102 +280,4 @@ class Router implements RouterInterface
         $this->routes[$this->count]['middlewares'][] = array('name' => $name, 'params' => $args);
     }
 
-    /**
-     * Set the class name
-     *
-     * @param string $class classname segment 1
-     *
-     * @return object Router
-     */
-    public function setClass($class)
-    {
-        $this->class = $class;
-        return $this;
-    }
-
-    /**
-     * Set current method
-     *
-     * @param string $method name
-     *
-     * @return object Router
-     */
-    public function setMethod($method)
-    {
-        $this->method = $method;
-        return $this;
-    }
-
-    /**
-     * Set the folder name
-     *
-     * @param string $folder folder
-     *
-     * @return object Router
-     */
-    public function setFolder($folder)
-    {
-        $this->folder = $folder;
-        return $this;
-    }
-
-    /**
-     * Get folder, folder name first letter must be uppercase
-     *
-     * @param string $separator get folder seperator
-     *
-     * @return string
-     */
-    public function getFolder($separator = '')
-    {
-        return (empty($this->folder)) ? '' : ucfirst($this->folder).$separator;
-    }
-
-    /**
-     * Returns to current routed class name
-     *
-     * @return string
-     */
-    public function getClass()
-    {
-        return ucfirst($this->class);  // class name first letter must be uppercase
-    }
-
-    /**
-     * Returns to current method
-     *
-     * @return string
-     */
-    public function getMethod()
-    {
-        return $this->method;
-    }
-
-    /**
-     * Returns php namespace of the current route
-     *
-     * @return string
-     */
-    public function getNamespace()
-    {
-        $folder = $this->getFolder();
-        if (strpos($folder, "/") > 0) {  // Converts "Tests\Welcome/home" to Tests\Welcome\Home
-            $exp = explode("/", $folder);
-            $folder = trim(implode("\\", $exp), "\\");
-        }
-        $namespace = $folder;
-        $namespace = trim($namespace, '\\');
-        return (empty($namespace)) ? '' : $namespace.'\\';
-    }
-
-    /**
-     * Clean dispatcher variables
-     *
-     * @return void
-     */
-    public function clear()
-    {
-        $this->class  = '';
-        $this->folder = '';
-    }
 }
